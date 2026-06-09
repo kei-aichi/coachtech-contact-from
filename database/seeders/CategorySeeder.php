cdc240a6068d3b9d46eca5aa4d99cf6a6f521438
keiichiro shikai <nb.jtktn.1211@gmail.com>
2026-06-09 12:07:54 +0900

    feat: category seeder and contact factory #3

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::insert([
+            ['content' => '商品のお届けについて'],
+            ['content' => '商品の交換について'],
+            ['content' => '商品トラブル'],
+            ['content' => 'ショップへのお問い合わせ'],
+            ['content' => 'その他'],
+        ]);
+    }
+}
